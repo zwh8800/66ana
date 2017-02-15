8b7f7f2c9ac22e4c6c981e2c5a26b8b0ef57372b
Zhang Weihu <496781108@qq.com>
2017-02-15 15:12:01 +0800

    add pull push work

--- service/zmq.go
+++ service/zmq.go
@@ -17,18 +17,20 @@ const (
 )
 
 var (
-	workerReportReqSocket *zmq.Socket = nil
-	workerReportRepSocket *zmq.Socket = nil
+	workerReportReqSocket  *zmq.Socket = nil
+	workerReportRepSocket  *zmq.Socket = nil
+	dispatchWorkPushSocket *zmq.Socket = nil
+	dispatchWorkPullSocket *zmq.Socket = nil
 )
 
 func GetWorkerReport() (*model.ReportPayload, error) {
 	if workerReportReqSocket == nil {
 		var err error
-		addr := fmt.Sprintf(zmqAddressFormat, conf.Conf.Zeromq.Addr, workerReportPort)
 		workerReportReqSocket, err = zmq.NewSocket(zmq.REQ)
 		if err != nil {
 			return nil, err
 		}
+		addr := fmt.Sprintf(zmqAddressFormat, conf.Conf.Zeromq.Addr, workerReportPort)
 		if err := workerReportReqSocket.Bind(addr); err != nil {
 			return nil, err
 		}
@@ -51,11 +53,11 @@ func GetWorkerReport() (*model.ReportPayload, error) {
 func ServeWorkerReport(reportCallback func(err error) *model.ReportPayload) error {
 	if workerReportRepSocket == nil {
 		var err error
-		addr := fmt.Sprintf(zmqAddressFormat, conf.Conf.Zeromq.Addr, workerReportPort)
 		workerReportRepSocket, err = zmq.NewSocket(zmq.REP)
 		if err != nil {
 			return err
 		}
+		addr := fmt.Sprintf(zmqAddressFormat, conf.Conf.Zeromq.Addr, workerReportPort)
 		if err := workerReportRepSocket.Connect(addr); err != nil {
 			return err
 		}
@@ -82,4 +84,55 @@ func ServeWorkerReport(reportCallback func(err error) *model.ReportPayload) erro
 		}
 	}()
 	return nil
+}
+
+func DispatchWork(payload *model.StartSpiderPayload) error {
+	if dispatchWorkPushSocket == nil {
+		var err error
+		dispatchWorkPushSocket, err = zmq.NewSocket(zmq.PUSH)
+		if err != nil {
+			return err
+		}
+		addr := fmt.Sprintf(zmqAddressFormat, conf.Conf.Zeromq.Addr, dispatchPort)
+		if err := dispatchWorkPushSocket.Bind(addr); err != nil {
+			return err
+		}
+	}
+
+	data, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	if _, err := dispatchWorkPushSocket.Send(string(data), 0); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func PullWork() (*model.StartSpiderPayload, error) {
+	if dispatchWorkPullSocket == nil {
+		var err error
+		dispatchWorkPullSocket, err = zmq.NewSocket(zmq.PULL)
+		if err != nil {
+			return nil, err
+		}
+		addr := fmt.Sprintf(zmqAddressFormat, conf.Conf.Zeromq.Addr, dispatchPort)
+		if err := dispatchWorkPullSocket.Connect(addr); err != nil {
+			return nil, err
+		}
+	}
+
+	data, err := dispatchWorkPullSocket.Recv(0)
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &model.StartSpiderPayload{}
+	if err := json.Unmarshal([]byte(data), payload); err != nil {
+		return nil, err
+	}
+
+	return payload, nil
 }
