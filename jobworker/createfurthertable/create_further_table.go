02ed7823d002abcb351abed92943a62e13dd81d4
Zhang Weihu <496781108@qq.com>
2017-03-18 23:43:28 +0800

    log

--- jobworker/createfurthertable/create_further_table.go
+++ jobworker/createfurthertable/create_further_table.go
@@ -7,7 +7,9 @@ import (
 )
 
 func CreateFurtherTable(interface{}) {
+	log.Println("Job CreateFurtherTable start")
 	if err := service.CreateFurtherTable(5); err != nil {
 		log.Println("service.CreateFurtherTable(5):", err)
 	}
+	log.Println("Job CreateFurtherTable end")
 }
