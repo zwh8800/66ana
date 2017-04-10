8eb3c78b2ef0e5e86ca81518e7f220f2169bc4ed
Zhang Weihu <496781108@qq.com>
2017-04-11 00:12:41 +0800

    fix: job entry

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	zmq "github.com/pebbe/zmq4"
 	"github.com/zwh8800/66ana/conf"
+	"github.com/zwh8800/66ana/jobworker"
 	"github.com/zwh8800/66ana/spiderworker"
 	"github.com/zwh8800/66ana/supervisor"
 	"github.com/zwh8800/66ana/web"
@@ -32,6 +33,10 @@ func main() {
 
 	if conf.Conf.Web.IsWeb {
 		go web.Run()
+	}
+
+	if conf.Conf.JobWorker.IsJobWorker {
+		go jobworker.Run()
 	}
 
 	ch := make(chan os.Signal, 1)
